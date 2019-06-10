03a7f288556de2092397b9f18401893c8388638f
Simon Legander <simon.legander@gmail.com>
2019-06-10 16:22:36 +0200

    Change docker image. Slim down config.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-set -ev
+set -e
 
 # rm -rf dist
 # mkdir dist
 # cp -r lib/* dist
+
+export AWS_ENV="production"
+export AWS_REGION="us-east-1"
+export AWS_ACCESS_KEY_ID=$PRODUCTION_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$PRODUCTION_SECRET_ACCESS_KEY
+
+if [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ]; then
+    echo "No AWS keys set. Exiting..."
+    exit
+fi
 
 # Change cname to lib.k6.io
 aws s3 sync --delete lib/ s3://k6-simon-cdn.loadimpact.com
