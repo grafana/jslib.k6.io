86413e6273fb0252334afbfb396dda839712b684
Simon Legander <simon.legander@gmail.com>
2019-06-11 10:28:19 +0200

    Add papaparse to basic test. Add package.JASON scripts.

--- tests/basic.js
+++ tests/basic.js
@@ -1,19 +1,31 @@
 import { check } from "k6";
 import jsonpath from "jslib.k6.io/jsonpath/1.0.2/index.js"
+import { parse, unparse } from "jslib.k6.io/papaparse/5.0.0/index.js"
 
 export const options = {
   "duration": "10s",
   "vus": 1
 };
 
-const testData = {
-  user: {
-    name: "Batman"
-  }
-};
+function testPapaParse() {
+  const data = `name,email,role\sBatman,batman@batman.email,superhero`;
+  check(data, {
+    "Papaparse works": () => unparse(parse(data))
+  });
+}
+
+function testJsonPath() {
+  const data = {
+    user: {
+      name: "Batman"
+    }
+  };
+  check(data, {
+    "JSON path works": () => jsonpath.value(data, 'user.name') === "Batman"
+  });
+}
 
 export default function() {
-  check(testData, {
-    "JSON path works": () => jsonpath.value(testData, 'user.name') === "Batman"
-  });
+  testPapaParse();
+  testJsonPath();
 }
