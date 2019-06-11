6fb7f9710fd4420a1143048a0613ae0b12c5c4d9
Simon Legander <simon.legander@gmail.com>
2019-06-11 14:31:46 +0200

    Update test.

--- tests/basic.js
+++ tests/basic.js
@@ -1,6 +1,7 @@
 import { check } from "k6";
-import jsonpath from "jslib.k6.io/jsonpath/1.0.2/index.js"
-import { parse, unparse } from "jslib.k6.io/papaparse/5.0.0/index.js"
+import jsonpath from "jslib.k6.io/jsonpath/1.0.2/index.js";
+import { parse, unparse } from "jslib.k6.io/papaparse/5.0.0/index.js";
+import formurlencoded from "jslib.k6.io/form-urlencoded/3.0.0/index.js";
 
 export const options = {
   "duration": "10s",
@@ -25,7 +26,19 @@ function testJsonPath() {
   });
 }
 
+function testFormurlencoded() {
+  const data = {
+    param1: "foo",
+    param2: "bar"
+  };
+
+  check(data, {
+    "form-urlencoded works": () => formurlencoded(data) === "param1=foo&param2=bar"
+  });
+}
+
 export default function() {
   testPapaParse();
   testJsonPath();
+  testFormurlencoded();
 }
