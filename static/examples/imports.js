b9fd07ad4bef2267a5262fed956f59564981f4df
Simon Legander <simon.legander@gmail.com>
2019-06-19 15:38:36 +0200

    Example copy update.

--- static/examples/imports.js
+++ static/examples/imports.js
@@ -1,5 +1,5 @@
 // Default import
 import jsonpath from "jslib.k6.io/jsonpath/1.0.2/index.js";
 
-// Single export import
+// Non default import
 import { nonDefaultExport } from "jslib.k6.io/some-js-pkg/1.0.0/index.js";
