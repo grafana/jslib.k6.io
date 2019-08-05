4769aa520ea633209f0bef4593e4e8014a0d215f
Simon Legander <simon.legander@gmail.com>
2019-08-05 10:50:47 +0200

    Update k6 to 0.25.0. Update examples to use scheme.

--- static/examples/full-script.js
+++ static/examples/full-script.js
@@ -1,5 +1,5 @@
 import { check } from "k6";
-import jsonpath from "jslib.k6.io/jsonpath/1.0.2/index.js"
+import jsonpath from "https://jslib.k6.io/jsonpath/1.0.2/index.js"
 
 export const options = {
   "duration": "10s",
