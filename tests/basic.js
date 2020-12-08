3ca3086952cfc260b0d43ee2668a3dcc43e0eeb2
Sin <sin.sin27100@gmail.com>
2020-12-08 16:51:01 +0700

    add bip39 library

--- tests/basic.js
+++ tests/basic.js
@@ -1,70 +1,74 @@
-import { check } from "k6";
-import { Counter } from 'k6/metrics';
+import { check } from 'k6'
+import { Counter } from 'k6/metrics'
 
-import jsonpath from "../lib/jsonpath/1.0.2/index.js";
-import formurlencoded from "../lib/form-urlencoded/3.0.0/index.js";
-import papaparse from "../lib/papaparse/5.1.1/index.js";
-
-import { randomIntBetween, randomItem, uuidv4 } from "../lib/k6-utils/1.0.0/index.js";
+import jsonpath from '../lib/jsonpath/1.0.2/index.js'
+import formurlencoded from '../lib/form-urlencoded/3.0.0/index.js'
+import papaparse from '../lib/papaparse/5.1.1/index.js'
+import { generateMnemonic } from '../lib/bip39/1.0.0/index.js'
+import { randomIntBetween, randomItem, uuidv4 } from '../lib/k6-utils/1.0.0/index.js'
 
 function testJsonPath() {
   const data = {
     user: {
-      name: "Batman"
-    }
-  };
+      name: 'Batman',
+    },
+  }
   check(data, {
-    "jsonpath works": () => jsonpath.value(data, 'user.name') === "Batman"
-  });
+    'jsonpath works': () => jsonpath.value(data, 'user.name') === 'Batman',
+  })
 }
 
 function testPapaparse() {
-  const csvString = "crocodileName;age\nBert;5";
+  const csvString = 'crocodileName;age\nBert;5'
   const config = {
-    header: true
-  };
+    header: true,
+  }
 
-  let parsed = papaparse.parse(csvString, config);
+  let parsed = papaparse.parse(csvString, config)
 
   check(parsed, {
-    "papaparse works": (data) =>  parsed.data[0].crocodileName === "Bert"
+    'papaparse works': (data) => parsed.data[0].crocodileName === 'Bert',
   })
 }
 
 function testFormurlencoded() {
   const data = {
-    param1: "foo",
-    param2: "bar"
-  };
+    param1: 'foo',
+    param2: 'bar',
+  }
 
   check(data, {
-    "form-urlencoded works": () => formurlencoded(data) === "param1=foo&param2=bar"
-  });
+    'form-urlencoded works': () => formurlencoded(data) === 'param1=foo&param2=bar',
+  })
 }
 
-function testRandomBetween(){
-  let randomInt = randomIntBetween(1,1);
-  console.log(randomInt);
+function testRandomBetween() {
+  let randomInt = randomIntBetween(1, 1)
+  console.log(randomInt)
   check(randomInt, {
-    "randomBetween works": (r) => r === 1,
-  });
+    'randomBetween works': (r) => r === 1,
+  })
 }
 
-function testRandomItem(){
-  let items = [1,2,3,4];
+function testRandomItem() {
+  let items = [1, 2, 3, 4]
 
   check(randomItem(items), {
-    "randomItem works": (item) => items.includes(item),
-  });
+    'randomItem works': (item) => items.includes(item),
+  })
 }
 
-function testuuidv4(){
-
+function testuuidv4() {
   check(uuidv4(), {
-    "uuidv4 works": (val) => val.length === 36,
-  });
+    'uuidv4 works': (val) => val.length === 36,
+  })
 }
 
+function testBip39() {
+  check(generateMnemonic(12), {
+    'mnemonic : ': (val) => console.log(val),
+  })
+}
 
 export {
   testJsonPath,
@@ -73,4 +77,5 @@ export {
   testRandomBetween,
   testRandomItem,
   testuuidv4,
+  testBip39,
 }
