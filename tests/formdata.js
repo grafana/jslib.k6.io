e5b6719e8a5d5817c2559017ba461719c77a74cf
Ivan Mirić <ivan@loadimpact.com>
2021-02-01 12:16:46 +0100

    Support appending http.FileData to FormData
    This doesn't work with ArrayBuffer yet, until https://github.com/loadimpact/k6/pull/1800
    is merged, so the current testSuiteBase will fail.
    
    Resolves https://github.com/loadimpact/jslib.k6.io/pull/21#pullrequestreview-579041818

--- tests/formdata.js
+++ tests/formdata.js
@@ -2,13 +2,13 @@ var http = require('k6/http');
 var check = require('k6').check
 var FormData = require('../lib/formdata/0.0.1/index.js').FormData;
 
-var logo1 = open('./data/logo.png', 'b');
+var logo = open('./data/logo.png', 'b');
 
 exports.testPost = function() {
   var fd = new FormData();
-  fd.append('binArray', logo1, 'logo1.png');
-  fd.append('ArrayBuffer', new Uint8Array(logo1).buffer, 'logo1.png');
-  fd.append('text', 'hello', 'hello.txt');
+  fd.append('binArray', http.file(logo, 'logo.png', 'image/png'));
+  fd.append('ArrayBuffer', http.file(new Uint8Array(logo).buffer, 'logo.png', 'image/png'));
+  fd.append('text', http.file('hello', 'hello.txt', 'text/plain'));
   var res = http.post('https://httpbin.test.k6.io/post', fd.body(),
                       { headers: { 'Content-Type': 'multipart/form-data; boundary=' + fd.boundary }});
   var checks = {
@@ -16,8 +16,8 @@ exports.testPost = function() {
     'files were uploaded correctly': function (res) {
       var files = res.json()['files'];
       return (Object.keys(files).length === 3 &&
-              (files['binArray'] && files['binArray'].includes('application/octet-stream')) &&
-              (files['ArrayBuffer'] && files['ArrayBuffer'].includes('application/octet-stream')) &&
+              (files['binArray'] && files['binArray'].includes('image/png')) &&
+              (files['ArrayBuffer'] && files['ArrayBuffer'].includes('image/png')) &&
               (files['binArray'] == files['ArrayBuffer']) &&
               (files['text'] == 'hello'));
     },
