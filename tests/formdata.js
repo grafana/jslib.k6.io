d32131cbd275d52cda188665182329404c2fd7bb
Ivan Mirić <ivan@loadimpact.com>
2021-03-18 11:36:42 +0100

    Make plain text form fields easier to use with FormData (#25)
    * Make plain text form fields easier to use with FormData
    
    Closes #23
    
    * Update FormData.append() docstring
    
    Resolves https://github.com/k6io/jslib.k6.io/pull/25#discussion_r594282005
    
    * Set content_type to text/plain if data is string
    
    Resolves https://github.com/k6io/jslib.k6.io/pull/25#discussion_r594395355
    
    * Use strict equality check for typeof

--- tests/formdata.js
+++ tests/formdata.js
@@ -1,18 +1,24 @@
 var http = require('k6/http');
 var check = require('k6').check
-var FormData = require('../lib/formdata/0.0.1/index.js').FormData;
+var FormData = require('../lib/formdata/0.0.2/index.js').FormData;
 
 var logo = open('./data/logo.png', 'b');
 
 exports.testPost = function() {
   var fd = new FormData();
   fd.append('binArray', http.file(logo, 'logo.png', 'image/png'));
-  fd.append('ArrayBuffer', { data:new Uint8Array(logo).buffer, filename: 'logo.png', content_type: 'image/png' });
+  fd.append('ArrayBuffer', { data: new Uint8Array(logo).buffer, filename: 'logo.png', content_type: 'image/png' });
   fd.append('text', http.file('hello', 'hello.txt', 'text/plain'));
+  fd.append('textField', 'world');
+  fd.append('anotherField', '!');
   var res = http.post('https://httpbin.test.k6.io/post', fd.body(),
                       { headers: { 'Content-Type': 'multipart/form-data; boundary=' + fd.boundary }});
   var checks = {
     'got 200 response': function (res) { return res.status === 200 },
+    'form fields were submitted correctly': function (res) {
+      var form = res.json()['form'];
+      return form.textField == 'world' && form.anotherField == '!';
+    },
     'files were uploaded correctly': function (res) {
       var files = res.json()['files'];
       return (Object.keys(files).length === 3 &&
