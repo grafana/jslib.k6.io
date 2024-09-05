32c36c2f5a91bb10817d9d77eba490d62ea79335
Lukasz Gut <blinku.dev@gmail.com>
2024-09-05 14:46:35 +0200

    Add missing test for k6chaijs@4.5.0.0

--- tests/k6chai.js
+++ tests/k6chai.js
@@ -2,6 +2,7 @@ import { expect as expect4340, describe as describe4340 } from '../lib/k6chaijs/
 import { expect as expect4341, describe as describe4341 } from '../lib/k6chaijs/4.3.4.1/index.js'
 import { expect as expect4342, describe as describe4342 } from '../lib/k6chaijs/4.3.4.2/index.js'
 import { expect as expect4343, describe as describe4343 } from '../lib/k6chaijs/4.3.4.3/index.js'
+import { expect as expect4500, describe as describe4500 } from '../lib/k6chaijs/4.5.0.0/index.js'
 import { expect as expect4501, describe as describe4501 } from '../lib/k6chaijs/4.5.0.1/index.js'
 
 export default function testk6chaijs() {
@@ -22,6 +23,10 @@ export default function testk6chaijs() {
 
   describe4343('k6chaijs v4.3.4.3 test', () => {
     expect4343(expected, alias).to.equal(expected)
+  })
+
+  describe4500('k6chaijs v4.5.0.0 test', () => {
+    expect4500(expected, alias).to.equal(expected)
   })
 
   describe4501('k6chaijs v4.5.0.1 test', () => {
