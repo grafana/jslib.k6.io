2b78c58608685f1a70debda78010c197e1aa45cd
codebien <2103732+codebien@users.noreply.github.com>
2025-05-21 14:44:13 +0200

    Updated deployment workflows

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,10 @@ set -e
 # mkdir dist
 # cp -r lib/* dist
 
-export AWS_ENV="production"
-export AWS_REGION="us-east-1"
-export AWS_ACCESS_KEY_ID=$PRODUCTION_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$PRODUCTION_SECRET_ACCESS_KEY
-
 if [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ]; then
     echo "No AWS keys set. Exiting..."
     exit
 fi
 
-# Change cname to lib.k6.io
-aws s3 sync --delete lib/ s3://jslib.k6.io --exclude "index.src.js"
-aws cloudfront create-invalidation --distribution-id EAWDELBNGDLKP --paths "/*"
+aws s3 sync --delete lib/ s3://${S3_BUCKET_ID} --exclude "index.src.js"
+aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths "/*"
